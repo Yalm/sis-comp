7e42f3f81ff888c6b67758cc061a16269bbc1e11
Yalm <renzomanuelc@gmail.com>
2019-02-04 23:46:27 -0500

    ...

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,7 +66,7 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
-            'key_file' =>  __DIR__.'\..\siscom.json',
+            'key_file' =>  __DIR__.'/../siscom.json',
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null),
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null),
